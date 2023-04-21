3a207305b64f4a94af837d294bd77992be2d2072
BuildTools <unconfigured@null.spigotmc.org>
2023-04-21 11:23:47 -0400

    Remove test plugin source, everything works as expected.

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,55 +1,17 @@
 #include "../plugin_sdk/plugin_sdk.hpp"
 
-// Declare plugin name & supported champions
-//
-PLUGIN_NAME("CarryGaren");
-SUPPORTED_CHAMPIONS(champion_id::Garen);
+PLUGIN_NAME("FirestormAIO");
 
-// Include champion file
-//
-#include "garen.h"
-
-// Entry point of plugin
-//
 PLUGIN_API bool on_sdk_load(plugin_sdk_core* plugin_sdk_good)
 {
-    // Global declaretion macro
-    //
     DECLARE_GLOBALS(plugin_sdk_good);
 
-    //Switch by myhero champion id
-    //
-    switch (myhero->get_champion())
-    {
-    case champion_id::Garen:
-        // Load garen script
-        //
-        garen::load();
-        break;
-    default:
-        // We don't support this champ, print message and return false (core will not load this plugin and on_sdk_unload will be never called)
-        //
-        console->print("Champion %s is not supported!", myhero->get_model_cstr());
-        return false;
-    }
+    console->print("Hello from Test Plugin");
 
-    // Return success, our plugin will be loaded now
-    //
     return true;
 }
 
-// Unload function, only when on_sdk_load returned true
-//
 PLUGIN_API void on_sdk_unload()
 {
-    switch (myhero->get_champion())
-    {
-    case champion_id::Garen:
-        // Unload garen script
-        //
-        garen::unload();
-        break;
-    default:
-        break;
-    }
+
 }
